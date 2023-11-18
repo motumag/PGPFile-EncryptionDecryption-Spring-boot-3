a3adf1365e4ebd9dc7d3606be32150b61e4b85bd
motumag <motumagishu27@gmail.com>
2023-11-18 14:41:21 +0300

    pgp encryption decryption file

---
+++ src/main/java/com/example/perfectpgpfileencdec/PerfectPgpFileEncDecApplication.java
@@ -0,0 +1,13 @@
+package com.example.perfectpgpfileencdec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfectPgpFileEncDecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PerfectPgpFileEncDecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/perfectpgpfileencdec/PerfectPgpFileEncDecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.perfectpgpfileencdec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfectPgpFileEncDecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
